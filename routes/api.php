44bbfe87a3a4107384c8cdfee15ea040e8458fae
Pablo Martinez <p_mart25@outlook.com>
2022-02-14 11:06:05 +0100

    update

--- routes/api.php
+++ routes/api.php
@@ -25,15 +25,13 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 
 Route::get('products', function() {
-    // If the Content-Type and Accept headers are set to 'application/json', 
-    // this will return a JSON structure. This will be cleaned up later.
+
     return Product::all();
 });
 
 
 Route::get('products/{id}', function($id) {
-    // If the Content-Type and Accept headers are set to 'application/json', 
-    // this will return a JSON structure. This will be cleaned up later.
+
     return Product::find($id);
 });
 
@@ -55,21 +53,18 @@ Route::post('updateamountproduct/{amountProduct}', [ProductController::class,'up
 Route::get('users', [UserController::class,'index']);
 Route::get('users/{id}', [UserController::class,'show']);
 Route::get('createuser/{userName},{email},{password},{deposit},{role}', [UserController::class,'createUser']);  //post
-//createuser/user1,user1@test.com,user1!password,20,buyer 
-//createuser/user2,user2@test.com,user2!password,20,seller
 
 
 
-// test 
 
 
-//login with user buyer
+
+
 
 //Route::post('register', 'App\Http\Controllers\Auth\RegisterController@register');
 Route::post('register', [RegisterController::class,'register']);
 
 
-//https://www.twilio.com/blog/build-restful-api-php-laravel-sanctum
 
 
 Route::post('register', [AuthController::class, 'register']);
@@ -92,7 +87,7 @@ Route::get ('reset/{userId}', [User::class, 'resetDeposit']);
 
 
 
-//https://www.amezmo.com/laravel-hosting-guides/role-based-api-authentication-with-laravel-sanctum
+
 Route::group(['middleware' => 'auth:sanctum'], function() {
     // list all post
     Route::post('deposit/{amountMoney}', [UserController::class, 'deposit']);
@@ -102,18 +97,3 @@ Route::group(['middleware' => 'auth:sanctum'], function() {
 
 });
 
-
-//Implement /deposit endpoint so users with a “buyer” role can deposit only 5, 10, 20, 
-//50 and 100 cent coins into their vending machine account (one coin at the time).  --> done 
-
-//Implement /buy endpoint (accepts productId, amount of products) so users with a
-//“buyer” role can buy a product (shouldn't be able to buy multiple different products
-//at the same time) with the money they’ve deposited. API should return total they’ve
-//spent, the product they’ve purchased and their change if there’s any (in an array of
-//5, 10, 20, 50 and 100 cent coins)  --> done
-
-//Implement /reset endpoint so users with a “buyer” role can reset their deposit back
-//to 0 --> done
-
-//Take time to think about possible edge cases and access issues that should be
-//solved
